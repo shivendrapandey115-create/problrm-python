984e84a8136d69d03a02b65d38aadb11e5687534
shivendrapandey115-create <shivendrapandey115@gmail.com>
2026-07-30 22:41:33 +0530

    added question 5 and question 6

---
+++ question5.py
@@ -0,0 +1,6 @@
+# Write a python program to find an average of two number 
+# entered n=by the user
+a = int(input("enter a number a:"))
+b = int(input("enter a number b:"))
+average = (a + b)/2
+print(average)
---
+++ question6.py
@@ -0,0 +1,5 @@
+# Write a python prongram to calculate the square of a number
+# entered ny the user
+a = int(input("enter a number a:"))
+squar = a*a
+print(squar)
